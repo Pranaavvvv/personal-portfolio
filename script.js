3612408258fec65ad291ae4195c42626ac76c737
Pranaavvvv <pranav.djsce24@gmail.com>
2024-10-21 11:20:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function startPortfolio() {
+    const clickSound = document.getElementById('clickSound');
+    clickSound.play();  // Play sound when clicking START button
+         window.location.href = "PP/index.html";
+}
